bcc64b3ea5179746db86267c17448f576d4adc25
Albert Safin <xzfcpw@gmail.com>
2018-10-29 03:08:51 +0700

    Work around crashing nim doc

--- ndb/sqlite.nim
+++ ndb/sqlite.nim
@@ -76,7 +76,8 @@
 import strutils, sqlite3, options
 
 import db_common
-export db_common
+when not defined(NimDoc): # https://github.com/nim-lang/Nim/issues/9140
+  export db_common
 
 type
   DbValueKind* = enum
